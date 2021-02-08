728d016f16664be06fd61ea4f2389e8690093bb6
Tara Matthew <taramatthew.93@gmail.com>
2021-02-08 17:34:37 +0000

    Amend styling for Recently Viewed component

--- resources/js/components/Home/RecentlyViewed.vue
+++ resources/js/components/Home/RecentlyViewed.vue
@@ -1,12 +1,11 @@
 <template>
-    <div class="border-t-2 border-l-2 border-r-2 border-b-2 border-red-500">
+    <div class="border-t-2 border-l-2 border-r-2 border-b-2 border-black ml-4 mt-4">
         <div class="border-b-2 border-black h-12 flex items-center justify-center"><h1>Recently Viewed</h1></div>
         <div class="h-without-header">
-            <div class="border-b-2 h-auto flex items-center min-h-1/4"><span>Sexy ramen</span></div>
-            <div class="border-b-2 h-auto flex items-center min-h-1/4"><span>Caroline's butt</span></div>
-            <div class="border-b-2 h-auto flex items-center min-h-1/4"><span>Sexy spaghetti</span></div>
-            <div class="h-auto flex items-center min-h-1/4"><span>Less sexy penne</span></div>
+            <div class="border-b-2 h-auto flex items-center min-h-1/4 p-4"><span>Sexy ramen</span></div>
+            <div class="border-b-2 h-auto flex items-center min-h-1/4 p-4"><span>Caroline's butt</span></div>
+            <div class="border-b-2 h-auto flex items-center min-h-1/4 p-4"><span>Sexy spaghetti</span></div>
+            <div class="h-auto flex items-center min-h-1/4 p-4"><span>Less sexy penne</span></div>
         </div>
     </div>
-
 </template>
