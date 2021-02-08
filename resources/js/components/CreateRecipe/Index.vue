24eaa5896dc9c77bedc06fa278d453918bd8af75
Tara Matthew <taramatthew.93@gmail.com>
2021-02-08 13:10:55 +0000

    Add header toolbar, placeholder Create Recipe component, Vue routing

---
+++ resources/js/components/CreateRecipe/Index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Create Recipe</h1>
+</template>
--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>Recipes</h1>
+    <nav class="mx-auto bg-green-400 p-5 flex justify-between">
+        <div>
+            <a>Recipes With Friends</a>
+        </div>
+        <ul class="flex flex-column">
+            <li class="pr-5"><router-link to="createRecipe">New Recipe</router-link></li>
+            <li class="pr-5">Random Recipe</li>
+            <li class="pr-5">Sign Out</li>
+        </ul>
+    </nav>
 </template>
