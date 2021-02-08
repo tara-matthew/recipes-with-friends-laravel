95fb843af17392df5507e544d7bd9a3d74919dff
Tara Matthew <taramatthew.93@gmail.com>
2021-02-08 19:12:11 +0000

    Amend array structure

--- resources/js/components/Home/HomeCard.vue
+++ resources/js/components/Home/HomeCard.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="border-t-2 border-l-2 border-r-2 border-b-2 border-black ml-4 mt-4">
-        <div class="border-b-2 border-black h-12 flex items-center justify-center"><h1>{{ data[0].title }}</h1></div>
+        <div class="border-b-2 border-black h-12 flex items-center justify-center"><h1>{{ data.title }}</h1></div>
         <div class="h-without-header">
-            <div class="border-b-2 h-auto flex items-center min-h-1/4 p-4"><span>{{ data[1].contents[0] }}</span></div>
-            <div class="border-b-2 h-auto flex items-center min-h-1/4 p-4"><span>{{ data[1].contents[1] }}</span></div>
-            <div class="border-b-2 h-auto flex items-center min-h-1/4 p-4"><span>{{ data[1].contents[2] }}</span></div>
-            <div class="h-auto flex items-center min-h-1/4 p-4"><span>{{ data[1].contents[3] }}</span></div>
+            <div class="border-b-2 h-auto flex items-center min-h-1/4 p-4"><span>{{ data.contents[0] }}</span></div>
+            <div class="border-b-2 h-auto flex items-center min-h-1/4 p-4"><span>{{ data.contents[1] }}</span></div>
+            <div class="border-b-2 h-auto flex items-center min-h-1/4 p-4"><span>{{ data.contents[2] }}</span></div>
+            <div class="h-auto flex items-center min-h-1/4 p-4"><span>{{ data.contents[3] }}</span></div>
         </div>
     </div>
 </template>
@@ -13,10 +13,6 @@
 <script>
     export default {
         props: {
-            // recentlyViewed: {
-            //     type: Boolean,
-            //     default: false
-            // },
             data: {}
         },
 
--- resources/js/components/Home/Index.vue
+++ resources/js/components/Home/Index.vue
@@ -3,28 +3,35 @@
     <div class="grid w-100 grid-cols-2 grid-rows-2 h-screen">
         <home-card v-if="data" :data="data.recentlyViewed"></home-card>
         <home-card v-if="data" :data="data.favourites"></home-card>
-        <home-card v-if="data" :data="data.favourites"></home-card>
-        <home-card v-if="data" :data="data.favourites"></home-card>
+        <home-card v-if="data" :data="data.recentlyAdded"></home-card>
+        <home-card v-if="data" :data="data.mostSearched"></home-card>
     </div>
 </template>
 
 <script>
-    import HomeCard from '@/js/components/Home/HomeCard'
+    import HomeCard from '@/js/components/Home/HomeCard';
 
     export default {
         data() {
             return {
-                data:
-                    {
-                        recentlyViewed: [
-                            {title: 'Recently Viewed'},
-                            {contents: ['Sexy ramen', 'Butt of Caroline', 'Sexy spaghetti', 'Less sexy penne']}
-                        ],
-                        favourites: [
-                            {title: 'Favourites'},
-                            {contents: ['Sweet Caroline']}
-                        ]
-                    }
+                data: {
+                    recentlyViewed: {
+                        title: 'Recently Viewed',
+                        contents: ['Sexy ramen', 'Butt of Caroline', 'Sexy spaghetti', 'Less sexy penne']
+                    },
+                    favourites: {
+                        title: 'Favourites',
+                        contents: ['Sweet Caroline']
+                    },
+                    recentlyAdded: {
+                        title: 'Recently Added',
+                        contents: ['Carolime Clark']
+                    },
+                    mostSearched: {
+                        title: 'Most Searched',
+                        contents: ['Carolime Clark', 'Carol', 'Food', 'Tasty']
+                    },
+                }
             }
         },
         mounted() {
