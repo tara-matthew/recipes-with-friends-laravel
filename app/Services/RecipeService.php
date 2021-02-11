9da9d488c061b64942251d6f0320b173c40ac033
Tara Matthew <taramatthew.93@gmail.com>
2021-02-11 15:09:48 +0000

    Amend Recipe Service code

--- app/Services/RecipeService.php
+++ app/Services/RecipeService.php
@@ -6,16 +6,23 @@ use App\Models\Recipe;
 
 class RecipeService
 {
+    /**
+     * Return all recipes with sorting options
+     * @return mixed
+     */
     public function retrieve()
     {
         $orderBy = 'created_at';
         $orderDirection = 'desc';
 
-        if (request()->get('sort')) {
-            $orderBy = request()->get('sort');
-        }
-        if (request()->get('direction')) {
-            $orderDirection = request()->get('direction');
+        //TODO This could probably go into a helper method if it's something I'll be doing in other parts of the app
+        if (count(request()->all())) {
+            if (request()->get('sort')) {
+                $orderBy = request()->get('sort');
+            }
+            if (request()->get('direction')) {
+                $orderDirection = request()->get('direction');
+            }
         }
 
         return Recipe::orderBy($orderBy, $orderDirection)->get();
