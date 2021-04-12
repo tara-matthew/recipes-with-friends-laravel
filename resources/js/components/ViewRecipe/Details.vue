cb71399e182a5b4f9c49e488b78cafd31e30878d
Tara Matthew <taramatthew.93@gmail.com>
2021-04-12 16:29:39 +0100

    Begin styling Details component

--- resources/js/components/ViewRecipe/Details.vue
+++ resources/js/components/ViewRecipe/Details.vue
@@ -1,10 +1,21 @@
 <template>
     <div class="border-t-2 border-l-2 border-r-2 border-b-2 border-black ml-4 mr-4 mt-4">
         <div class="border-b-2 border-black h-12 flex items-center justify-center"><h1> {{ recipe.title }}</h1></div>
-        <div class="flex flex-col overflow-y-auto">
-            <div class="border-b-2 items-center p-4 last:border-b-0 min-h-0">
-                <p>Story - {{ recipe.story }}</p>
-                <p>Photo - {{ recipe.main_photo }}</p>
+        <div>
+            <div class="border-b-2 p-4">
+                <div class="image-container w-full md:w-2/5 sm:w-full h-72 border-black border-2 inline-flex items-center justify-center">
+                    <p class="font-bold">Photo</p>
+                </div>
+                <div class="details-container w-full md:w-3/5 sm:w-full h-72 border-black border-2 inline-flex align-top">
+                    <div class="mt-52 w-6/12 text-center">
+                        <p class="font-bold">Prep: 20 mins</p>
+                        <p class="font-bold">Cook: 30 mins</p>
+                    </div>
+                    <div class="mt-52 w-6/12 text-center">
+                        <p class="font-bold">Cuisine: Japanese</p>
+                        <p class="font-bold">Mood: Sexy</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
