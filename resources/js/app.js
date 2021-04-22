4aa4e16f2222d3671d2dc338bf4282cceecdfebb
Tara Matthew <taramatthew.93@gmail.com>
2021-04-22 08:49:02 +0100

    Register OuterContainer globally

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import router from '@/js/routes.js';
 import App from '@/js/views/App';
 import PageHeader from '@/js/components/Header';
 import Panel from '@/js/components/Panel';
+import OuterContainer from '@/js/components/OuterContainer';
 const app = createApp(App);
 
 app.use(router);
@@ -11,5 +12,6 @@ app.use(router);
 //Register the Header component globally
 app.component('PageHeader', PageHeader);
 app.component('Panel', Panel);
+app.component('OuterContainer', OuterContainer);
 
 app.mount('#app');
